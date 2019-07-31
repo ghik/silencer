50444b70b30b9979a263690f714bfb6c3d277902
ghik <romeqjanoosh@gmail.com>
2019-07-31 11:56:49 +0200

    fixed travis release

--- travis/release.sh
+++
@@ -1,3 +0,0 @@
-openssl aes-256-cbc -K $encrypted_c1f2aadc729a_key -iv $encrypted_c1f2aadc729a_iv -in secrets.tar.enc -out local.secrets.tar -d
-tar xv -C travis -f local.secrets.tar
-sbt +publishSigned sonatypeRelease
